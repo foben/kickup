a7628995e7258cabf6d6bde22a1b083d71de0bae
muetoph <muetoph@example.com>
2019-03-19 23:26:39 +0100

    removed the limit of 100 games for the elo table

--- kickup/persistence.py
+++ kickup/persistence.py
@@ -69,9 +69,4 @@ def save_kickup_match(kickup):
 def matches_sorted():
     matches = [Match(**match) for match in mongo().matches.find()]
     sorted_matches = sorted(matches, key=lambda m: m.date)
-    count = len(sorted_matches)
-    if count < 101:
-        return sorted_matches
-    else:
-        return sorted_matches[count-100:]
-
+    return sorted_matches
